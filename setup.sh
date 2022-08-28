505098888f0ae16a7c27c7bed28adc2e9034efc2
ThienAn783 <53578281+ThienAn783@users.noreply.github.com>
2022-08-28 21:45:21 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,11 +1,11 @@
 mkdir -p ~/.streamlit/
 echo "[theme]
-primaryColor="#575fe8"
-backgroundColor="#f5f7f3"
-secondaryBackgroundColor="#c9eab8"
-font = "sans serif"
+primaryColor='#575fe8'
+backgroundColor='#f5f7f3'
+secondaryBackgroundColor='#c9eab8'
+font = 'sans serif'
 [server]
 headless = true
 port = $PORT
 enableCORS = false
-" > ~/.streamlit/config.toml
+ > ~/.streamlit/config.toml

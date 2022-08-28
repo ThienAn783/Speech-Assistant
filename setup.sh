2b7555c60824aac0056b262910c9e8d420bdc5bc
ThienAn783 <53578281+ThienAn783@users.noreply.github.com>
2022-08-28 21:37:42 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,8 +1,8 @@
 mkdir -p ~/.streamlit/
 echo "[theme]
-primaryColor=’#575fe8’
-backgroundColor=’#f5f7f3’
-secondaryBackgroundColor=’#c9eab8’
+primaryColor=‘#575fe8’
+backgroundColor=‘#f5f7f3’
+secondaryBackgroundColor=‘#c9eab8’
 font = ‘sans serif’
 [server]
 headless = true

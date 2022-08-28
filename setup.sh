dca00b718c279a852cfb91c5f9cf96392ab3c810
ThienAn783 <53578281+ThienAn783@users.noreply.github.com>
2022-08-28 21:40:14 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,9 @@
 mkdir -p ~/.streamlit/
 echo "[theme]
-primaryColor=‘#575fe8’
-backgroundColor=‘#f5f7f3’
-secondaryBackgroundColor=‘#c9eab8’
-font = ‘sans serif’
+primaryColor="#575fe8"
+backgroundColor="#f5f7f3"
+secondaryBackgroundColor="#c9eab8"
+font = "sans serif"
 [server]
 headless = true
 port = $PORT
